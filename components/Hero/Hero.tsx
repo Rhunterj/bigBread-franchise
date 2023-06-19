df15514b067c508fbd3759410fdaf147054bc3aa
Robbin <robbin_jagt@hotmail.com>
2023-06-19 09:01:30 +0200

    fix linmk

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface HeroProps {
     image: string
@@ -17,12 +18,12 @@ export const Hero = ({ image }: HeroProps) => {
             <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-[#1c1c1c]" />
             <div className="absolute bottom-0 left-0 flex items-center justify-center w-full h-1/2">
                 <div className="flex flex-col items-center justify-center min-w-[50%] h-1/2">
-                    <a
+                    <Link
                         className="px-8 py-2 mt-4 text-xs font-bold text-white rounded-[10px] xl:text-3xl bg-primary xl:py-6"
                         href="/#contactForm"
                     >
                         Vraag de brochure aan{' '}
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
