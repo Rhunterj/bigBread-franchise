b5c1207b750530e96b29af0931e7333575ec95d2
Robbin <robbin_jagt@hotmail.com>
2023-06-29 18:33:09 +0200

    changes

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ interface HeroProps {
 
 export const Hero = ({ image }: HeroProps) => {
     return (
-        <div className="relative w-full h-64 border-t-4 border-white bg-gradient-to-t to-black xl:h-[700px]">
+        <div className="relative w-full h-64 border-t-4 border-white bg-gradient-to-t to-black sm:h-[700px]">
             <Image
                 src={image}
                 alt="Hero image"
@@ -19,7 +19,7 @@ export const Hero = ({ image }: HeroProps) => {
             <div className="absolute bottom-0 left-0 flex items-center justify-center w-full h-1/2">
                 <div className="flex flex-col items-center justify-center min-w-[50%] h-1/2">
                     <Link
-                        className="px-8 py-2 mt-4 text-xs font-bold text-white rounded-[10px] xl:text-xl bg-primary xl:py-4"
+                        className="px-8 py-2 mt-4 text-xs font-bold text-white rounded-[10px] sm:text-xl bg-primary sm:py-4"
                         href="/#contactForm"
                     >
                         Vraag de brochure aan{' '}
